2e020af1619b52c1e0656b3adcb18a3bb9d7cc92
Marcos Motta <mrcsmotta1@gmail.com>
2024-07-28 22:59:06 -0300

    Alteração para salvar os dados no BD

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Utilities\FileHelper;
 use App\Services\ExcelService;
 use App\Utilities\DirectoryHelper;
 use App\Http\Requests\StoreUploadRequest;
+use App\Repositories\EloquentInvoiceRepository;
+
+
 
 class UploadController extends Controller
 {
     public $arrayData;
-    public function __construct(private ExcelService $excelService)
+    public function __construct(private ExcelService $excelService, private EloquentInvoiceRepository $invoiceRepository)
     {
         $this->arrayData = [];
     }
@@ -23,14 +27,19 @@ class UploadController extends Controller
 
         $file = $request->file('excel_file');
         $fileName = $file->getClientOriginalName();
-        $file->storeAs('import', $fileName, 'public');
+        $filePath = $file->storeAs('import', $fileName, 'public');
         $directoryPath = storage_path('app/public/import') . "/{$fileName}";
 
         $this->arrayData = $this->excelService->processSpreadsheet($directoryPath);
 
         if (empty($this->arrayData)) {
+            FileHelper::moveAndDeleteFile($filePath, $fileName, __CLASS__, __FUNCTION__, true);
             return back()->withErrors(['message' => 'Erro no processamento do arquivo!'])->withInput();
         }
+
+        $this->invoiceRepository->add($this->arrayData);
+
+        FileHelper::moveAndDeleteFile($filePath, $fileName, __CLASS__, __FUNCTION__);
 
         return redirect()->back()->with('success', 'Arquivo de cobrança enviado com sucesso!');
     }
